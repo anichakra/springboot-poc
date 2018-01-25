3d1c7161700d3dc684fc034bb6a62844a83742fb
JSingh <JSingh@elementcorp.com>
2018-01-25 11:12:17 +0530

    Added changes to resolve kafka config

--- some-app-service/src/main/java/com/anycompany/someapp/kafka/config/KafkaProducerConfig.java
+++ some-app-service/src/main/java/com/anycompany/someapp/kafka/config/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,10 +16,13 @@ import org.springframework.kafka.core.ProducerFactory;
 @Configuration
 public class KafkaProducerConfig {
 
+	@Value("kafka-host")
+	private String kafkaHost;
+	
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> producerConfigProperties = new HashMap<>();
-		producerConfigProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		producerConfigProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost);
 		producerConfigProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		producerConfigProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(producerConfigProperties);
