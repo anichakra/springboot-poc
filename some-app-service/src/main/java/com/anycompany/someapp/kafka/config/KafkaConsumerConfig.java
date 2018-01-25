6606deeade9048b3fcc2b8edfa33623cbc0c61a2
JSingh <JSingh@elementcorp.com>
2018-01-25 12:10:58 +0530

    Changes for kafka host

--- some-app-service/src/main/java/com/anycompany/someapp/kafka/config/KafkaConsumerConfig.java
+++ some-app-service/src/main/java/com/anycompany/someapp/kafka/config/KafkaConsumerConfig.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,14 +17,11 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @EnableKafka
 public class KafkaConsumerConfig {
 
-	@Value("kafka-host")
-	private String kafkaHost; 
-
 	@Bean
 	@ConditionalOnMissingBean(ConsumerFactory.class)
 	public ConsumerFactory<Object, Object> consumerFactory() {
 		Map<String, Object> consumerConfigProperties = new HashMap<>();
-		consumerConfigProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost);
+		consumerConfigProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		consumerConfigProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "group-someFeed");
 		consumerConfigProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		consumerConfigProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
--- some-app-service/src/main/java/com/anycompany/someapp/kafka/config/KafkaProducerConfig.java
+++ some-app-service/src/main/java/com/anycompany/someapp/kafka/config/KafkaProducerConfig.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,14 +14,11 @@ import org.springframework.kafka.core.ProducerFactory;
 
 @Configuration
 public class KafkaProducerConfig {
-
-	@Value("kafka-host")
-	private String kafkaHost;
 	
 	@Bean
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> producerConfigProperties = new HashMap<>();
-		producerConfigProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaHost);
+		producerConfigProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		producerConfigProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		producerConfigProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(producerConfigProperties);
